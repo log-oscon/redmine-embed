ef9c355b4d9ecd9b21cdb972735e888a541e0162
Luis Rodrigues <hello@goblindegook.net>
2015-06-03 14:49:38 +0100

    Initial commit

---
+++ lib/Deactivator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    PluginName
+ * @subpackage PluginName/includes
+ */
+
+namespace logoscon\WP\RedmineEmbed;
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    PluginName
+ * @subpackage PluginName/includes
+ * @author     Your Name <email@example.com>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ redmine-embed.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Redmine Embed
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Redmine Embed
+ * Plugin URI:        https://github.com/log-oscon/redmine-embed
+ * Description:       Embed Redmine issues in your posts and pages.
+ * Version:           1.0.0
+ * Author:            log.OSCON, Lda.
+ * Author URI:        http://log.pt/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       redmine-embed
+ * Domain Path:       /languages
+ */
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+    require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+use logoscon\WP\RedmineEmbed;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in lib/Activator.php
+ */
+\register_activation_hook( __FILE__, '\logoscon\WP\RedmineEmbed\Activator::activate' );
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in lib/Deactivator.php
+ */
+\register_deactivation_hook( __FILE__, '\logoscon\WP\RedmineEmbed\Deactivator::deactivate' );
+
+/**
+ * Begins execution of the plugin.
+ *
+ * @since    1.0.0
+ */
+\add_action( 'plugins_loaded', function () {
+    $plugin = new RedmineEmbed\Plugin();
+    $plugin->run();
+} );
